efa0cb3756b54eb02a654540d8a9afe429bb1fd6
i-MINGYAN.LEE <I-MINGYAN.LEE@deltaww.com>
2024-02-26 17:05:05 +0800

    update url sult logic

--- sultData.py
+++ sultData.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import nums_from_string as nfs
 
-testData=open('testData.html','r',encoding="utf-8")
+testData=open('グランブルーファンタジー.html','r',encoding="utf-8")
 soup=BeautifulSoup(testData, 'html.parser')
 items=soup.find_all('div',class_='lis-item treasure se')
 ids=[]
 qs=[]
 for item in items:
-    ids.append(item.find('img')['src'].replace('https://prd-game-a-granbluefantasy.akamaized.net/assets/img/sp/assets/item/article/s/','').replace('.jpg',''))
+    ids.append(nfs.get_nums(item.find('img')['src'])[0])
     qs.append(item.getText().strip())
 dict={'id':ids,'quantity':qs}
 df=pd.DataFrame(dict)
